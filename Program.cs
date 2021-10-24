495bb98b91041c5583b8b2893dabcebd25330fe8
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-24 18:51:39 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PesoIdeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        { 
+            altura dupla , pesoM , pesoF;
+            string M =  "M" , F =  "F",sexo;
+
+            Console.Write ("Digite sua altura em m:");
+            altura = Convert.ToDouble ( Console.ReadLine ());
+            Console . Write ("Sexo [M] asculino / [F]eminino:");
+            sexo =  Console.ReadLine();
+
+            pesoM = altura * 72.7 - 58.0;
+            pesoF = altura * 62.1 - 44.7;
+
+            if ( sexo ==  M )
+            {
+        
+            Console . WriteLine ($"Seu peso ideal é: {pesoM : N1} kg");
+            }
+
+            else  if (sexo == F)
+            {
+            Console.WriteLine ($" Seu peso ideal é: {pesoF : N1} kg");
+
+          }
+        }
+    }
+}
